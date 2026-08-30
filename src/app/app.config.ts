e2cc26d91b8694dfba102efca834781128a754cf
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-08-30 23:51:48 +0200

    feat: add personal data vertical slice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { PersonalDataRepositoryPort } from './features/client/personal-data/domain/interfaces/personal-data-repository.port';
+import { PersonalDataMockRepository } from './features/client/personal-data/infrastructure/api/personal-data-mock.repository';
+import { PersonalDataEffects } from './features/client/personal-data/state/personal-data.effects';
+import { personalDataFeature } from './features/client/personal-data/state/personal-data.feature';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideState(personalDataFeature),
+    provideEffects(PersonalDataEffects),
+    {
+      provide: PersonalDataRepositoryPort,
+      useClass: PersonalDataMockRepository,
+    },
+  ],
 };
